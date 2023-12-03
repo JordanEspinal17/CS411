91f10aa0f7633a2748f75a2569ac65f6e397c53c
JordanEspinal17 <112999535+JordanEspinal17@users.noreply.github.com>
2023-12-03 13:24:04 -0500

    Add files via upload
    FULL RUNNING BACKEND CODE HERE

---
+++ backend/demo.py
@@ -0,0 +1,139 @@
+import os
+import base64
+from requests import post, get
+import json
+
+## imbd code 
+
+import requests
+test = []
+
+def get_movie_details(api_key, search_query):
+    search_url = f'https://api.themoviedb.org/3/search/movie'
+
+    genres_url = f'https://api.themoviedb.org/3/genre/movie/list?api_key={api_key}'
+
+    try:
+        # Make a GET request to the TMDb API for movie search
+        response = requests.get(search_url, params={'api_key': api_key, 'query': search_query})
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+
+            # Extract information
+            results = data['results']
+            if results:
+                first_result = results[0]
+                movie_details = {
+                    'title': first_result['title'],
+                    'overview': first_result['overview'],
+                    'release_date': first_result['release_date'],
+                }
+
+                # Get the genre names instead of IDs
+                genre_ids = first_result['genre_ids']
+                genres_response = requests.get(genres_url)
+                genres_data = genres_response.json()
+                genre_names = [genre['name'] for genre in genres_data['genres'] if genre['id'] in genre_ids]
+                movie_details['genres'] = genre_names
+
+                return movie_details['genres']
+            else:
+                return f"No results found for the movie title: {search_query}"
+        else:
+            return f"Error: Unable to perform movie search. Status Code: {response.status_code}"
+
+    except requests.RequestException as e:
+        return f"Error: {e}"
+
+api_key = ''
+
+search_queries = input("Enter movie titles separated by commas: ").split(',')
+
+for search_query in search_queries:
+    result = get_movie_details(api_key, search_query)
+    test.extend(result)
+
+
+## get from .env
+
+client_id = ""
+client_secret = ""
+
+
+# print(client_id, client_secret)
+
+def get_token():
+    auth_string = client_id + ":" + client_secret #retrieve authorization token
+    auth_bytes = auth_string.encode("utf-8") #Encoded
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8") #Encoded using base 64 
+
+    url = "https://accounts.spotify.com/api/token"
+    header = {
+        "Authorization": "Basic " + auth_base64, 
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=header, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+#Contructing the header
+def get_auth_header (token):
+    return {"Authorization": "Bearer " + token}
+
+def search_for_artist(token, artist_name):
+    url = "https://api.spotify.com/v1/search"
+    headers = get_auth_header(token)
+    query = f"?q={artist_name}&type=artist&limit=1" #any values you're looking for
+
+    query_url = url + query
+    result = get(query_url, headers=headers)
+    json_result = json.loads(result.content)["artists"]["items"]
+    # print(json_result)
+    if len(json_result) == 0:
+        print("No artist with this name exist...")
+        return None
+    return json_result[0]
+
+
+def get_song_by_artist(token, artist_id):
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = get_auth_header(token)
+    result = get(url, headers=headers)
+    json_result = json.loads(result.content)["tracks"]
+    return json_result
+
+def  get_song_by_genre(token, seed_genres_list):
+    base_url = "https://api.spotify.com/v1/recommendations"
+    seed_genres = ','.join(seed_genres_list)  # Convert list of genres to comma-separated string
+    url = f"{base_url}?seed_genres={seed_genres}"
+    headers = get_auth_header(token)
+    
+    result = get(url, headers=headers)
+    tracks = json.loads(result.content).get("tracks", [])
+    
+    return tracks
+
+
+
+
+
+token = get_token()
+# result = search_for_artist(token, "Taylor Swift")
+# artist_id = result["id"]
+# songs_by_artist = get_song_by_artist(token, artist_id)
+# for idx, song in enumerate(songs_by_artist): #enumerate is a built-in Python function that returns both the index and the value of each item in a list (or other iterable).
+#     print(f"{idx + 1}. {song['name']}")
+
+#genres = ["rock"] #case sensitive
+
+
+genres = [string.lower() for string in test] #case sensitive 
+print(genres)
+songs = get_song_by_genre(token, genres )
+for idx, song in enumerate(songs):  # `enumerate` is a built-in Python function that returns both the index and the value of each item in a list (or other iterable).
+    print(f"{idx + 1}. {song['name']}")  # This line prints the song's name with its corresponding index (starting from 1, not 0).
+
